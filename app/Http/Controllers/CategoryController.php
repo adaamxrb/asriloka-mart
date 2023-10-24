4eac74c3874334749f31a80698467813c3f60303
adaamxrb <adamroybafiel@gmail.com>
2023-10-24 11:00:28 +0700

    feat/categories: merge ready

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+use App\Models\Product;
+
 class CategoryController extends Controller
 {
     /**
@@ -13,6 +16,26 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('pages.category');
+        $categories = Category::all(); // Get all categories
+        $products = Product::with(['galleries'])->paginate(32); // Get all products with relation to galleries table
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products
+        ]);
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function detail(Request $request, $slug)
+    {
+        $categories = Category::all(); // Get all categories
+        $category = Category::where('slug', $slug)->firstOrFail(); // Get category by slug
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->paginate(32); // Get all products with relation to galleries table
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products
+        ]);
     }
 }
